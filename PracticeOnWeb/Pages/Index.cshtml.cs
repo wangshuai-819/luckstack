e7736c33ef184a45da0562a106d2f6aa7987d4d0
王帅 <1685909098@qq.com>
2020-04-22 12:33:19 +0800

    repository练习

--- PracticeOnWeb/Pages/Index.cshtml.cs
+++ PracticeOnWeb/Pages/Index.cshtml.cs
@@ -18,38 +18,67 @@ namespace PracticeOnWeb.Pages
         //    _logger = logger;
         //}
         #endregion
-        
+        public IndexModel()
+        {
+            _problemRepository = new ProblemRepository();
+        }
+        private ProblemRepository _problemRepository;
         public IEnumerable<Problem> ProblemLists { get; set; }
         public void OnGet()  //html只会取onget 方法中的属性值   即要求model声明属性  
         {
-            
-            ProblemLists = new List<Problem>
-            {
-                  new Problem
-                {
-                    PublishTime = DateTime.Now,
-                    Author = new User { Name = " 那是花开911", Id = 1 },
-                    Title = "ASP 网站迁移",
-                    Id = 1,
-                    Abstract = "在服务器上把原先的asp网站整体迁移出来，在新的服务器上重新布置……"
+            ProblemLists = _problemRepository.Get();
 
-                },
-                     new Problem
-                {
-                    PublishTime = DateTime.Now,
-                    Author = new User { Name = "  txdyr", Id = 2 },
-                    Title = " Winform里控件绑定数据源就会报错",
-                    Id = 2,
-                    Abstract = " 项目里的控件绑定属性都会出现这个问题…… "
-
-                },
-             };
+        }
+        public void OnPost()
+        {
 
         }
     }
     public class ProblemRepository
     {
-
+        private static IList<Problem> _problems;
+        static ProblemRepository()
+        {
+            _problems = new List<Problem>
+            {
+                  new Problem
+                {
+                    PublishTime = DateTime.Now,
+                    Author = new User { Name = " 那是花开911", Id = 111 },
+                    Title = "ASP 网站迁移",
+                    Id = 1,
+                    Abstract = "在服务器上把原先的asp网站整体迁移出来，在新的服务器上重新布置……",
+                    Status=ProblemStatus.Cancelled
+                },
+                     new Problem
+                {
+                    PublishTime = DateTime.Now,
+                    Author = new User { Name = "  txdyr", Id = 222 },
+                    Title = " Winform里控件绑定数据源就会报错",
+                    Id = 2,
+                    Abstract = " 项目里的控件绑定属性都会出现这个问题…… ",
+                    Status=ProblemStatus.Rewarded,
+                },
+             };
+        }
+        public IList<Problem> Get()
+        {
+            return _problems;
+        }
+        public void Add(Problem problem)
+        {
+            _problems.Add(problem);
+        }
+        public void Delete(Problem problem)
+        {
+            _problems.Remove(problem);
+        }
+    }
+    public enum ProblemStatus
+    {
+        Cancelled,
+        InProcess,
+        Rewarded,
     }
     public class Problem
     {
@@ -58,6 +87,7 @@ namespace PracticeOnWeb.Pages
         public User Author { get; set; }
         public int Id { get; set; }
         public string Abstract { get; set; }
+        public ProblemStatus Status { get; set; }
     }
     public class User
     {
